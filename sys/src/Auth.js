58280fc4098e565162390d3f0a932422f0593f3d
Lucas Cassiano Silva <lucascassiano@users.noreply.github.com>
2018-10-23 16:21:11 -0300

    express + registration page

--- sys/src/Auth.js
+++ sys/src/Auth.js
@@ -1,11 +1,16 @@
 import { db } from "./Database.js";
 
 export async function ValidateEditKey(id, edit_key) {
-    let result = await db.one(`SELECT edit_key FROM teams WHERE id=${id}`);
-    if (edit_key == result.edit_key && result.edit_key != null)
-        return true;
-    else
+    try {
+        let result = await db.one(`SELECT edit_key FROM teams WHERE id=${id}`);
+        if (edit_key == result.edit_key && result.edit_key != null)
+            return true;
+        else
+            return false;
+    }
+    catch (e) {
         return false;
+    }
 }
 
 export async function ValidateViewKey(id, view_key) {
@@ -21,8 +26,12 @@ export async function ValidateRequest(teamId, editKey, viewKey) {
     let canEdit = false;
     let canView = false;
 
-    canView = await ValidateViewKey(teamId, viewKey);
-    canEdit = await ValidateEditKey(teamId, editKey);
+    try {
+        canView = await ValidateViewKey(teamId, viewKey);
+        canEdit = await ValidateEditKey(teamId, editKey);
+    } catch (e) {
+
+    }
 
     return ({ canEdit, canView });
 }
--- sys/src/index.js
+++ sys/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+//import reload from 'express-reload';
 
 //GraphQL dependencies
 import graphqlHTTP from 'express-graphql';
@@ -11,10 +13,11 @@ import { ValidateRequest } from "./Auth";
 import React from "react";
 import { renderToString } from 'react-dom/server'; // <-- renderToString() will take our React app and turn it into a string to be inserted into our Html template function.
 import { ServerStyleSheet } from 'styled-components'; // <-- importing ServerStyleSheet
-import App from './views/App';
-import Html from './views/Html';
 
-var path = require('path');
+import App from './client/App';
+import Html from './client/Html';
+//import Home from "./client/Home.html";
+
 var app = express();
 app.use(cors());
 
@@ -23,20 +26,27 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
+//hot reload
+var clientPath = path.join(__dirname, '/client/');
+//app.use(reload(clientPath));
+
+var chokidar = require('chokidar');
+var watcher = chokidar.watch(clientPath);
+watcher.on('ready', function () {
+    watcher.on('all', function () {
+        console.log("Clearing /app/ module cache from server")
+        Object.keys(require.cache).forEach(function (id) {
+            if (/[\/\\]app[\/\\]/.test(id)) delete require.cache[id]
+        })
+    })
+})
+
 /*** Home ***/
 app.get('/', async (req, res) => {
     const sheet = new ServerStyleSheet(); // <-- creating out stylesheet
     const body = renderToString(sheet.collectStyles(<App />)); // <-- collecting styles
     const styles = sheet.getStyleTags(); // <-- getting all the tags from the sheet
-    const title = 'Server side Rendering with Styled Components';
-
-    let teamId = req.query.id;
-    let editKey = req.query.edit;
-    let viewKey = req.query.view;
-
-    let keysAuth = await ValidateRequest(teamId, editKey, viewKey);
-
-    console.log(keysAuth);
+    const title = 'Home';
 
     res.send(
         Html({
@@ -45,24 +55,31 @@ app.get('/', async (req, res) => {
             title
         })
     );
-
 });
 
-/*** Team ***/
-app.get('/startup', (req, res) => {
-    const body = renderToString(<App />);
-    const title = 'Server side Rendering with Styled Components';
-    res.send(
-        Html({
-            body,
-            title
-        })
-    );
+app.use('/public', express.static(__dirname + '/client/public'));
+
+app.use('/home', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/home.html'));
 });
 
-app.use('/static', express.static(__dirname + '/views/team/static'));
-app.use('/team', (req, res) => {
-    res.sendFile(path.join(__dirname + '/views/team/index.html'));
+app.use('/register', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/register.html'));
+});
+
+app.use('/static', express.static(__dirname + '/client/team/static'));
+
+app.use('/team', async (req, res) => {
+    let teamId = req.query.id;
+    let editKey = req.query.edit;
+    let viewKey = req.query.view;
+    let keysAuth = await ValidateRequest(teamId, editKey, viewKey);
+
+    console.log(keysAuth);
+    if (!keysAuth.canEdit && !keysAuth.canView)
+        res.redirect('/home');
+
+    res.sendFile(path.join(__dirname + '/client/team/index.html'));
 });
 
 const port = process.env.PORT;
