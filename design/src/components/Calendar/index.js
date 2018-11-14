ec34ad07864cb18f684cf5c793fa2527d2c38205
AllanSCosta <allanc@mit.edu>
2018-11-14 21:10:46 +0100

    update calendar

--- design/src/components/Calendar/index.js
+++ design/src/components/Calendar/index.js
@@ -18,7 +18,7 @@ export default class Calendar extends Component {
     render() {
         return <div>
             <iframe
-                src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;&amp;src=kglc6gceolerre2i2bmg145i3o%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FFortaleza"
+                src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;&amp;src=kglc6gceolerre2i2bmg145i3o%40group.calendar.google.com&amp;color=%23182C57&amp;ctz=America%2FBrasilia"
                 width="100%"
                 height="250"
                 frameborder="0"
